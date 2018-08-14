61fd8e218de6fca6e26ba8ef86da84f4f7494fd4
Trevor <litseyt@me.com>
2018-08-13 21:50:36 -0500

    add sentry

--- src/pages/CashFlow/helpers.spec.js
+++ src/pages/CashFlow/helpers.spec.js
@@ -1,26 +1,28 @@
-import moment from 'moment'
+import moment from 'moment';
 import { trimOldOneTimeExpenses } from './helpers';
 
 it('trimOldOneTimeExpenses should trim expenses with start dates less than today', () => {
-	const oldIdToDelete = 'six'
+	const oldIdToDelete = 'six';
 
 	const oneTimeExpenses = {
 		[oldIdToDelete]: {
 			name: 'yayya!!!',
 			startDate: moment().subtract(1, 'day'), // minus one day
-			amount: -110000
+			amount: -110000,
 		},
 		two: {
 			name: 'check this out',
 			startDate: moment().add(1, 'day'), // plus one day
 			amount: 11,
-		}
-	}
+		},
+	};
 
 	const trimmedExpenses = trimOldOneTimeExpenses({ ...oneTimeExpenses });
 
 	// one less than before
-	expect(Object.keys(trimmedExpenses).length).toEqual(Object.keys(oneTimeExpenses).length - 1);
+	expect(Object.keys(trimmedExpenses).length).toEqual(
+		Object.keys(oneTimeExpenses).length - 1
+	);
 
 	// should be gone
 	expect(trimmedExpenses[oldIdToDelete]).toBeUndefined();
